521a55307623260c3d5ee88d335d75f85bebfde8
zsfzsd <46658419+zsfzsd@users.noreply.github.com>
2026-08-03 16:00:10 +0800

    test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import plotly.graph_objects as go
+import pandas as pd
 
+st.title("最小化测试")
+df = pd.DataFrame({'x': [1, 2, 3], 'y': [2, 4, 1]})
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df['x'], y=df['y'], mode='lines'))
+st.plotly_chart(fig)
